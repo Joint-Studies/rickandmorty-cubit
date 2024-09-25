c53804b8c7e6cca73a75584ba27a79162e6a1749
Manoel Amâncio <manoel_amancio@hotmail.com>
2024-09-24 23:55:05 -0300

    fix lint

--- rick_and_morty_cubit/lib/features/characters/presentation/cubit/characters_state.dart
+++ rick_and_morty_cubit/lib/features/characters/presentation/cubit/characters_state.dart
@@ -14,11 +14,11 @@ class CharactersLoading extends CharactersState {}
 class CharactersLoaded extends CharactersState {
   final ResponseEntity responseEntity;
 
-  CharactersLoaded({required this.responseEntity});
+  const CharactersLoaded({required this.responseEntity});
 }
 
 class CharactersError extends CharactersState {
   final String? msgError;
 
-  CharactersError({required this.msgError});
+  const CharactersError({required this.msgError});
 }
